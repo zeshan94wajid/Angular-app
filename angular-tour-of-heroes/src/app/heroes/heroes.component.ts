d6bf7092de841709e260a7954b0e21395d20764d
Zeshan Wajid <zeshan.wajid@crowdproperty.com>
2020-02-14 11:25:28 +0000

    allow user to select and view details of a single hero

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,17 +8,22 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  //define a hero variable and assign value
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
