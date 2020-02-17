c1339f14d09b5b831ec5c5eda359574b8f757995
Zeshan Wajid <zeshan.wajid@crowdproperty.com>
2020-02-17 15:00:33 +0000

    handle errors

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -5,6 +5,8 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import {catchError, map, tap} from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -20,7 +22,10 @@ export class HeroService {
   // }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   private log(message: string) {
@@ -30,6 +35,16 @@ export class HeroService {
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find( hero => hero.id === id));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 
 }
